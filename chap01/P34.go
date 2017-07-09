327ba03a9dd3e95e048a4e97565a12c5f66d7755
nabetama <mao.nabeta@gmail.com>
2017-07-10 00:05:40 +0900

    fmt

--- chap01/P34.go
+++ chap01/P34.go
@@ -7,7 +7,7 @@ import (
 
 type P34 struct {
 	Numbers []int
-	Want    int   // この数に出来るか
+	Want    int // この数に出来るか
 }
 
 func NewP34(numbers []int) *P34 {
@@ -17,13 +17,13 @@ func NewP34(numbers []int) *P34 {
 }
 
 func (p *P34) solve(idx, sum int) bool {
-	if idx == p.isLast() {	// 最後までチェックした？
+	if idx == p.isLast() { // 最後までチェックした？
 		return p.Want == sum
 	}
-	if p.solve(idx + 1, sum){
+	if p.solve(idx+1, sum) {
 		return true
 	}
-	if p.solve(idx + 1, sum + p.Numbers[idx]) {
+	if p.solve(idx+1, sum+p.Numbers[idx]) {
 		return true
 	}
 	return false
@@ -42,7 +42,7 @@ func (p *P34) isLast() int {
 }
 
 func main() {
-	numbers := NewP34([]int{1,2,3,7})
+	numbers := NewP34([]int{1, 2, 3, 7})
 	if numbers.pickUpToBecome(19) {
 		fmt.Println("dekiru!")
 		os.Exit(0)
